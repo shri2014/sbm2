ab94ceb253a2572eea2dca0911de32b57d403fda
Shrikant <shrikant@hp.com>
2017-01-17 17:02:03 -0800

    1st Version

---
+++ src/main/java/org/shri/sbm/App.java
@@ -0,0 +1,13 @@
+package org.shri.sbm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
